67b1840691f63d66701a29474b14bb298f399dab
rahmankhattak711 <rahmankhattak796@gmail.com>
2025-01-28 16:32:10 +0500

    situp Dark and Ligth Theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Poppins, DM_Serif_Text } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -31,7 +32,14 @@ export default function RootLayout({
         <body
           className={`${poppins.variable} ${dmSerifText.variable} antialiased`}
         >
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ModeToggle";
+
 export default function Home() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <div className="w-1/2 inline-flex justify-center items-center text-center">
-        <h1 className="text-4xl tracking-tighter  font-dmSerifText text-orange-500 uppercase">
+    <div>
+      <div>
+        <h1 className="text-2xl tracking-tighter  font-dmSerifText text-orange-500 uppercase">
           Welcome to Socially WEB App!
         </h1>
+        <Button>Sign In</Button>
+
+        <ModeToggle />
       </div>
     </div>
   );
