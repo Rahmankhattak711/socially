3283c1a8fc8b5526e9cd7493fcb66e2051f05a68
rahmankhattak711 <rahmankhattak796@gmail.com>
2025-01-29 16:07:44 +0500

    adding prisma and clerk and store user to the db

---
+++ src/app/action/userAction.ts
@@ -0,0 +1,31 @@
+"use server";
+import { prisma } from "@/lib/prisma";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+export async function syncUser() {
+  try {
+    let { userId } = await auth();
+    let user = await currentUser();
+
+    if (!user || !userId) return;
+
+    let existingUser = await prisma.user.findUnique({
+      where: {
+        clerkId: userId,
+      },
+    });
+
+    if (existingUser) return existingUser;
+
+    let createUser = await prisma.user.create({
+      data: {
+        clerkId: userId,
+        name: `${user.firstName || ""} ${user.lastName || ""}`,
+        username:
+          user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+        email: user.emailAddresses[0].emailAddress,
+        image: user.imageUrl,
+      },
+    });
+  } catch (error) {}
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,20 @@ import Link from "next/link";
 import { currentUser } from "@clerk/nextjs/server";
 import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
+import { syncUser } from "@/app/action/userAction";
 
 async function Navbar() {
   const user = await currentUser();
+
+  if(user ) await syncUser()
 
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <Link href="/" className="text-xl font-bold text-primary tracking-wider">
+            <Link href="/"
+              className="text-xl font-bold text-primary tracking-wider">
               Socially
             </Link>
           </div>
