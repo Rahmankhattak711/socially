50989f51f5656a21e279f982614036bb567f2074
Rahmankhattak711 <rahmankhattak796@gmail.com>
2025-11-27 01:25:29 +0500

    feat : update navbar logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import { currentUser } from "@clerk/nextjs/server";
 import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
 import { syncUser } from "@/app/action/userAction";
+import Image from "next/image";
 
 async function Navbar() {
   const user = await currentUser();
@@ -13,10 +14,18 @@ async function Navbar() {
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
-            <Link href="/"
-              className="text-xl font-bold text-primary tracking-wider">
-              Socially
+         <div className="flex items-center">
+            <Link href="/" className="flex items-center gap-2">
+              <Image
+                src="/assets/logo.jpg"
+                alt="logo"
+                width={40}
+                height={40}
+                className="rounded-md shadow-sm"
+              />
+              <span className="text-xl font-bold tracking-wide">
+                Socially
+              </span>
             </Link>
           </div>
 
